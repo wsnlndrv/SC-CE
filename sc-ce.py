c69cac02bbe275eb3a6c164a8b8259a3b0645a99
wsnlndr <53552337+wsnlndrv@users.noreply.github.com>
2023-03-18 11:09:32 +0000

    Update sc-ce.py

--- sc-ce.py
+++ sc-ce.py
@@ -1,10 +1,46 @@
 #!/usr/bin/python
 # -*- coding: utf-8; -*-
 
-import a2s
+import threading, time, a2s, os, sys, datetime
+import pandas as pd
 
-l1 = a2s.players("0.0.0.0", 30815)
-l2 = a2s.info("0.0.0.0", 30815)
+nombre_servidor = "  Nombre del servidor"
+filigrana = "**********************************"
+IPDS = "0.0.0.0"
+PUERTEISHON = 27015
 
-print(*l1, sep = "\n")
-print(*l2, sep = "\n")
+class Base(threading.Thread):
+    def __init__(self):
+        self._timer_runs = threading.Event()
+        self._timer_runs.set()
+        super().__init__()
+        
+    def iniciar(self):
+        while self._timer_runs.is_set():
+            self.timer()
+            time.sleep(self.__class__.interval)
+
+    def parar(self):
+        self._timer_runs.clear()
+    
+    def mostrar(self):
+        os.system('clear')
+        print(filigrana+"\n"+nombre_servidor+"\n"+filigrana)
+        IP = (IPDS, PUERTEISHON)
+        df = pd.DataFrame(a2s.players(IP))
+        
+        try:
+            columna1 = df.iloc[:,1]
+            print("\n".join(usuario[1] for usuario in columna1))
+        except IndexError:
+            print("Nadie conectado.")
+
+class Consultar(Base):
+    interval = 60
+    def timer(self):
+        base = Base()
+        base.mostrar()
+
+consultar = Consultar()
+consultar.iniciar()
+consultar.parar()
